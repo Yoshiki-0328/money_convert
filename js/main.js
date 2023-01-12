b0e00d02abba98b479ea6076de6109915ce63b86
Yoshiki-0328 <defeat.kj.y@outlook.jp>
2023-01-13 07:44:50 +0900

    js 為替取得機能作成 Please enter the commit message for your changes. Lines starting

--- js/main.js
+++ js/main.js
@@ -4,6 +4,7 @@ const $yen = document.getElementById('jp-yen');
 const $dol = document.getElementById('us-dol');
 
 let rate = 130; //ドル円為替
+const url ='http://www.gaitameonline.com/rateaj/getrate'//API
 
 $yen.addEventListener('input',()=>{
     let val = currencyChange($yen.value);
@@ -18,9 +19,12 @@ function currencyChange(val){
 // 為替取得
 async function fetchRate(){
     const response = await fetch('rate.json');
-    const json = response.json;
-    console.log(json);
-
+    const json = await response.json();
+    const datas = json.quotes;
+    for (let data of datas){
+        if(data.currencyPairCode=='USDJPY'){
+            rate=data.bid;
+        };
+    }
 }
-
 fetchRate();
